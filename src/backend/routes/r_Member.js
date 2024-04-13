a2ddb5759b36135ec364cf9cb9322c4b044514d9
thanadech6509650435 <thanadech.par@dome.tu.ac.th>
2024-04-13 23:32:15 +0700

    update payment

--- src/backend/routes/r_Member.js
+++ src/backend/routes/r_Member.js
@@ -1,12 +1,15 @@
 const { Router } = require('express')
-const Member = require('../database/crud/Member')
-const router = require('./r_Auth')
 const pool = require('../database/connect');
 const { update } = require('../database/crud/AuthEmployee');
-
+const Menu = require('../database/crud/Menu')
+const Member = require('../database/crud/Member')
+const PayMent = require('../database/crud/Payment')
+const router = require('./r_Auth');
+const Payment = require('../database/crud/Payment');
+//edit profile
 router.post('/member/update', (req, res) => {
-    const { memberID, gender, name, email, tel, join_date, birthday, points, street, subdistrict, district, city, zipcode } = req.body;
-    Member.update(memberID, gender, name, email, tel, join_date, birthday, points, street, subdistrict, district, city, zipcode)
+    const { gender, name, emial, tel, street, subdistrict, district, city, zipcode } = req.body;
+    Member.edit_profile(gender, name, emial, tel, street, subdistrict, district, city, zipcode, memberID)
         .then(result => {
             res.status(200);
             res.json(new Message('Success', 'Updated'));
@@ -17,8 +20,8 @@ router.post('/member/update', (req, res) => {
             res.json(new Message("Fail", err));
         })
 });
-
-router.post('/member/point', (req, res) => {
+//check current points
+router.post('/member/points', (req, res) => {
     const { memberID, points } = req.body;
     Member.findByID(memberID)
         .then(result => {
@@ -28,4 +31,28 @@ router.post('/member/point', (req, res) => {
             }
             res.json(new Message('Success', 'Have points', result.points))
         })
+});
+//see menu
+router.get('/member/menu', (req, res) => {
+    Menu.findAll().then(result => {
+        res.status(400);
+    })
+})
+//check payment
+router.get('/member/payment', (req, res) => {
+    const { paymentID, orderID, method, amount, status, datetime, successdatetime } = req.body;
+    Payment.update(paymentID, orderID, method, amount, status, datetime, successdatetime)
+        .then(result => {
+            res.status(200)
+            res.json(new Message('Success', 'Already Paid'))
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400);
+            res.json(new Message("Fail", 'Have not paid yet'));
+        })
+})
+//exchange coupon
+router.post('/member/exchange', (req, res) => {
+
 })
